2c974dc6f40bced4c21e59bc71c08e9ae53de1e6
Waqar Ali <skyvz21@gmail.com>
2022-07-08 18:04:40 +0400

    package name corrected

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 Route::group(['namespace' => 'Waqarali7\Imilezcart\Http\Controllers'],function (){
-    Route::get('imilezcart/test', 'ImilezcartController@test');
-//    Route::get('imilezcart/test', function (){
-//        dd("test");
-//    });
+//    Route::get('imilezcart/test', 'ImilezcartController@test');
+    Route::get('imilezcart/test', function (){
+        dd("test");
+    });
 });
